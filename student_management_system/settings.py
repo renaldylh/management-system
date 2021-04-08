a93209006abe4d4bb3b187c097b675913207aa82
renaldy luthfiana hakim <renaldyluthfiana123@gmail.com>
2021-04-08 16:44:35 +0700

    settins

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -155,3 +157,4 @@ EMAIL_FILE_PATH=os.path.join(BASE_DIR,"sent_mails")
 # import dj_database_url
 # prod_db=dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
